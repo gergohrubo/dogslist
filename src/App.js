adb594aba296ea39d3a0ff4de33d790501216a62
gergohrubo <gergohrubo@gmail.com>
2019-11-21 14:35:09 +0100

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DogsListContainer from './components/DogsListContainer'
+import DogBreedImages from './components/DogBreedImages'
+import DogBreedImagesContainer from './components/DogBreedImagesContainer'
+import { Route } from "react-router-dom";
 
 function App() {
   return (
@@ -10,6 +14,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Route exact path="/" component={DogsListContainer} />
+        <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
         <a
           className="App-link"
           href="https://reactjs.org"
